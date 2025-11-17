f107c2d44e92a5c9670e12bb7634bc1eaa51bacd
Hansi Senevirathna <englishbasictobrilliance@gmail.com>
2025-11-17 16:51:38 +0000

    see still not white background

--- src/components/sections/BulkPackagingSection.tsx
+++ src/components/sections/BulkPackagingSection.tsx
@@ -8,7 +8,7 @@ export default function BulkPackagingSection() {
     const privateLabelImage = PlaceHolderImages.find(p => p.id === 'private-labeling');
     
     return (
-        <section id="packaging" className="py-20 md:py-28 bg-background">
+        <section id="packaging" className="py-20 md:py-28 bg-white">
             <div className="container">
                 <div className="grid lg:grid-cols-2 gap-16 lg:gap-8 items-center">
                     
